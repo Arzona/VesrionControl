86ae3119a3c42a2d1f9221a1795e781428a18a5c
Áron Lakatos <aron.lakatos@stud.uni-corvinus.hu>
2020-09-24 19:36:35 +0200

    vegleges

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -22,6 +22,7 @@ namespace UserMaintenance
         
             button1.Text = Resource1.Add;
             button2.Text = Resource1.File;
+            button3.Text = Resource1.Delete;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -59,6 +60,13 @@ namespace UserMaintenance
                    
                 }
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+         
+
+        
         }
     }
 }
