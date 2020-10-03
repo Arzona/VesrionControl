6afe219d81a128fc8ddef376c8ec457eab95afa4
Áron Lakatos <aron.lakatos@stud.uni-corvinus.hu>
2020-10-03 14:35:51 +0200

    asd

--- 4gyak_excel/4gyak_excel/Form1.cs
+++ 4gyak_excel/4gyak_excel/Form1.cs
@@ -27,7 +27,31 @@ namespace _4gyak_excel
             InitializeComponent();
             LoadData();
             CreateExcel();
+            GetCell();
         }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int divided = y;
+            int modulo;
+
+            while (divided > 0)
+            {
+                modulo = (divided - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                divided = (int)((divided - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+
+            
+
+            
+        }
+
+       
 
         private void CreateExcel()
         {
@@ -59,7 +83,45 @@ namespace _4gyak_excel
 
         private void CreateTable()
         {
-            throw new NotImplementedException();
+            string[] headers = new string[] {
+             "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = f.Price/f.FloorArea;
+                counter++;
+
+               
+            }
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
         }
 
         private void LoadData()
