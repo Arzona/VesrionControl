16db855fb7b890936e0572da0b4833a9abfe7a09
Áron Lakatos <aron.lakatos@stud.uni-corvinus.hu>
2020-10-18 18:54:14 +0200

    6.feladat

--- 06gyak_webszolgaltatas/06gyak_webszolgaltatas/Form1.cs
+++ 06gyak_webszolgaltatas/06gyak_webszolgaltatas/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace _06gyak_webszolgaltatas
@@ -25,7 +26,26 @@ namespace _06gyak_webszolgaltatas
             InitializeComponent();
             GetExchangeRates();
             GetXml();
+            GetDiagram();
             dataGridView1.DataSource = Rates;
+            chartRateData.DataSource = Rates;
+        }
+
+        private void GetDiagram()
+        {
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private void GetXml()
