61b096099521268cb9ba87502ed020057c3146a8
Áron Lakatos <aron.lakatos@stud.uni-corvinus.hu>
2020-10-25 18:36:49 +0100

    3. feladat

---
+++ 07_gyak_mikroszimulacio/07_gyak_mikroszimulacio/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_gyak_mikroszimulacio.Entities
+{
+    public class BirthProbability
+    {
+        public int Kor { get; set; }
+        public int Gyremekekszama { get; set; }
+        public double P { get; set; }
+    }
+}
