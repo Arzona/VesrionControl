841fe5b3c11a4793315589255e79ca66e399da4f
Áron Lakatos <aron.lakatos@stud.uni-corvinus.hu>
2020-11-15 16:20:31 +0100

    14. feladat

---
+++ 8_gyak_fejlesztesi_mintak/8_gyak_fejlesztesi_mintak/Entities/Present.cs
@@ -0,0 +1,18 @@
+﻿using _8_gyak_fejlesztesi_mintak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_gyak_fejlesztesi_mintak.Entities
+{
+    class Present : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
