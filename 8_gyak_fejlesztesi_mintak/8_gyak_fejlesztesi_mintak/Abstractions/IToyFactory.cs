1e402ef7a3b3064fdd9007cfa2dd8a9b2ac2952a
Áron Lakatos <aron.lakatos@stud.uni-corvinus.hu>
2020-11-15 15:35:13 +0100

    8. feladat

---
+++ 8_gyak_fejlesztesi_mintak/8_gyak_fejlesztesi_mintak/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_gyak_fejlesztesi_mintak.Abstractions
+{
+    interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
